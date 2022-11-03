5500daf00e990fa48ffd337ade13745885f429e0
Bobby <pfsystem.brown@gmail.com>
2022-11-02 20:14:59 -0500

    s

--- src/converters/Model.ts
+++ src/converters/Model.ts
@@ -1,7 +1,11 @@
 import { DMMF } from "@prisma/generator-helper";
 import { logger } from "@prisma/internals";
-
+import { IField } from "../interfaces/IField";
 export const genModel = async (options: DMMF.Model) => {
     logger.info(JSON.stringify(options));
+
+    const fields: IField[] = options.fields.map((f): IField => {
+        return { name, type };
+    });
     return JSON.stringify(options);
 };
--- src/interfaces/IField.ts
+++ src/interfaces/IField.ts
@@ -1,5 +1,11 @@
+import { DefaultPrismaFieldType } from "../types";
 export interface IField {
     name: string;
+    pk: boolean;
     unique: boolean;
     kind: "scalar" | "object";
+    type: DefaultPrismaFieldType;
+    required: boolean;
+    readonly: boolean;
+    decorations: string;
 }
